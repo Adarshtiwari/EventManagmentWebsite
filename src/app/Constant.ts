f9c21fa8d99c015929ba52b4430394ace18291f7
Adarsh Tiwari <adarsh.tiwari@jktech.com>
2023-12-20 15:15:19 +0530

    remove constant from git ignore

---
+++ src/app/Constant.ts
@@ -0,0 +1,2 @@
+export const eventRequest=" https://q3bni9xn6j.execute-api.us-east-1.amazonaws.com/dev/createevent"
+export const getALLEventData=" https://m33jawx949.execute-api.us-east-1.amazonaws.com/getallevent"
